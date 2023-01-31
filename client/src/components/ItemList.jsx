473c1550c5ee780234d9b3768f8fd757e125dd3e
janine7492 <janine.supercode@gmail.com>
2023-01-31 17:16:55 +0100

    Home finished + ItemCard finished + ItemList in progress

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import ItemCard from "./ItemCard";
 
 const ItemList = () => {
+
+  const cat = useParams().category;
+
+  const [items, setItems] = useState([]);
+
   return (
-    <div>ItemList</div>
+    <div>
+      {items.map((item) => {
+        return <ItemCard img={item.img} name={item.name} room={item.room} />
+      })}
+    </div>
   )
 }
 export default ItemList
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,29 +1,31 @@
 import bigPic from "../assets/img/bigStuff.jpg";
 import mediumPic from "../assets/img/mediumStuff.jpg";
 import smallPic from "../assets/img/smallStuff.jpg";
+import bgi from "../assets/img/bgi.jpg";
 import BackButton from "../components/BackButton";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <section>
+      <section className="headerHome">
+        <img src={bgi} alt="Interior Design Sample" />
         <h1>MY FURNITURE</h1>
       </section>
-      <section>
-        <article>
+      <section className="mainHome">
+        <article className="thumbnailCat">
           <img src={bigPic} alt="Sofa in a living room" />
           <h2>BIG STUFF</h2>
         </article>
-        <article>
+        <article className="thumbnailCat">
           <img src={mediumPic} alt="Chair and laundry bag" />
           <h2>NOT SO BIG STUFF</h2>
         </article>
-        <article>
+        <article className="thumbnailCat">
           <img src={smallPic} alt="Decoration items on a shelf" />
           <h2>SMALL STUFF</h2>
         </article>
       </section>
-      <BackButton />
     </div>
   )
 }
