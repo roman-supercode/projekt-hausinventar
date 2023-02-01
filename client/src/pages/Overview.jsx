f6f01ed2894a5a07d4808ef55fe9eb75d3663ceb
roman-supercode <roman.supercode@gmail.com>
2023-02-01 13:19:50 +0100

    post route

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,7 +1,17 @@
+import AddItem from "../components/AddItem";
+import { useParams } from "react-router-dom";
+import ItemList from "../components/ItemList";
 
 const Overview = () => {
+
+  const cat = useParams().category;
+
   return (
-    <div>Overview</div>
+    <div>Overview
+      <AddItem />
+      <ItemList cat={cat} />
+
+    </div>
   );
 };
 export default Overview;
